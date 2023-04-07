9d87fa1a233f8d30d97ced7f3fb42e2237b0aa8a
PrathameshBelvalkar <125945246+PrathameshBelvalkar@users.noreply.github.com>
2023-04-07 13:17:02 +0530

    First Commit

---
+++ js/custom.js
@@ -0,0 +1,63 @@
+(function ($) {
+  "use strict";
+
+  // COLOR MODE
+  $(".color-mode").click(function () {
+    $(".color-mode-icon").toggleClass("active");
+    $("body").toggleClass("dark-mode");
+  });
+
+  // HEADER
+  $(".navbar").headroom();
+
+  // PROJECT CAROUSEL
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 10,
+    nav: true,
+  });
+  //disable right click
+  // document.addEventListener("contextmenu", (event) => {
+  //   event.preventDefault();
+  //   alert("Right Click Disabled");
+  // });
+  //Disable ctrl+u
+  document.onkeydown = function (e) {
+    if (
+      e.ctrlKey &&
+      (e.keyCode === 67 ||
+        e.keyCode === 86 ||
+        e.keyCode === 85 ||
+        e.keyCode === 117)
+    ) {
+      alert("Not Allowed");
+      return false;
+    } else {
+      return true;
+    }
+  };
+  //Kursor
+  new kursor({
+    type: 1,
+    removeDefaultCursor: true
+  });
+  // SMOOTHSCROLL
+  $(function () {
+    $(".nav-link, .custom-btn-link").on("click", function (event) {
+      var $anchor = $(this);
+      $("html, body")
+        .stop()
+        .animate(
+          {
+            scrollTop: $($anchor.attr("href")).offset().top - 49,
+          },
+          1000
+        );
+      event.preventDefault();
+    });
+  });
+
+  // TOOLTIP
+  $(".social-links a").tooltip();
+})(jQuery);
